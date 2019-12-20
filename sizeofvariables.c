2c7e54bff636d260ad6ed2c651b8ce5f0ba3a5ca
Laurelwyvern <58751456+Laurelwyvern@users.noreply.github.com>
2019-12-19 21:01:18 -0800

    Update sizeofvariables.c

--- sizeofvariables.c
+++ sizeofvariables.c
@@ -1,15 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
   int a = 545;
-  char b = "545";
-  float c = 545;
+  char b = '5';
+  float c = 545.0;
   double d = 545;
 
   // print value and size of an int variable
-  printf("int a value: %d and size: %d bytes\n", a, sizeof(a));
-  printf("char b value: %d and size: %d bytes\n", b, sizeof(b));
-  printf("float c value: %d and size: %d bytes\n", c, sizeof(c));
-  printf("double d value: %d and size: %d bytes\n", d, sizeof(d));
+  printf("int a value: %d and size: %lu bytes\n", a, sizeof(a));
+  printf("char b value: %d and size: %lu bytes\n", b, sizeof(b));
+  printf("float c value: %f and size: %lu bytes\n", c, sizeof(c));
+  printf("double d value: %f and size: %lu bytes\n", d, sizeof(d));
 }
