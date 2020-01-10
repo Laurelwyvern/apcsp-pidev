1796782040140853af20fbbbc10decc584f74d3b
Laurelwyvern <58751456+Laurelwyvern@users.noreply.github.com>
2020-01-09 21:52:20 -0800

    Update areaofcircle.c

--- areaofcircle.c
+++ areaofcircle.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+float areaOfCircle(float radius)
+{
+  float area;
+  area = radius*radius*3.14;
+  return area;
+}
+
+int main()
+{
+  float i;
+  for (i=3.5; i<13; i++)
+  {
+    printf("area: %f, radius: %f\n", areaOfCircle(i), i);
+  }
+}
